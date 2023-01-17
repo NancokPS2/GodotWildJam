ab76e0f6e3981a3c983bde8c9dccad08a7a21d49
NancokPS2 <nancok@tuta.io>
2023-01-17 18:09:57 -0300

    Cambios en Estructura

--- Clases/Obstaculos/ParedDestruible.gd
+++ Clases/Obstaculos/ParedDestruible.gd
@@ -1,8 +1,6 @@
-extends Pared
+extends Estructura
 class_name ParedDestruible
 
-export (int) var salud = 10
 
-func damage(cantidad:int,incomingDirection:Vector2=Vector2.ZERO):
-	salud -= cantidad
+
 	
