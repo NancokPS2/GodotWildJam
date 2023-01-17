5e60f17dfa348a37443a6b1eb1292afd8ff61231
NancokPS2 <nancok@tuta.io>
2023-01-16 21:46:51 -0300

    Los controles de las entidades solo functionan si controlando es verdadero

--- Clases/Entidades/Entidad.gd
+++ Clases/Entidades/Entidad.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 class_name Entidad
 
+var controlando:bool = false setget set_controlando #Solo deberia aceptar input si esto es true
+
 var temperatura:int 
 var inmortal:bool = false
 export (float) var salud = 50
@@ -10,6 +12,10 @@ var temperaturaFusion = 50
 func check_temperatura(delta):
 	if temperatura > temperaturaFusion:
 		hurt( (temperaturaFusion - temperatura) * delta )
+
+func set_controlando(valor):
+	controlando = valor
+	set_process_unhandled_input(valor)
 
 func _physics_process(delta: float) -> void:
 	check_temperatura(delta)
