b0eaf958f6c7a810989590a3e8b53cebb6080b2e
NancokPS2 <nancok@tuta.io>
2023-01-19 18:23:02 -0300

    Mejorado el funcionamiento de la grua

--- Objetos/Grua/GruaDeslizante.gd
+++ Objetos/Grua/GruaDeslizante.gd
@@ -1,10 +1,12 @@
 extends Entidad
 class_name GruaDeslizante
 
-export (float) var velocidad = 35
+export (float) var velocidad = 70
 
 func _ready() -> void:
 	controlando = true
+
+var miscMultiplier:float = 1
 
 func _physics_process(delta: float) -> void:
 	
@@ -14,11 +16,14 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("move_right"):
 		$Core.move_and_slide(Vector2.RIGHT * velocidad)
 		
-	elif Input.is_action_pressed("move_down"):
-		$Core/Brazo.move_and_slide(Vector2.UP * velocidad)
+	if Input.is_action_pressed("move_down"):
+		$Core/GarraGrua.move_and_slide(Vector2.UP * velocidad)
 		
 	elif Input.is_action_pressed("move_up"):
-		$Core/Brazo.move_and_slide(Vector2.DOWN * velocidad)
+		$Core/GarraGrua.move_and_slide(Vector2.DOWN * velocidad)
+		
+	$Core/BrazoSprite.position.y = $Core/GarraGrua.position.y / 2
+	$Core/BrazoSprite.scale.y = $Core/GarraGrua.position.y * miscMultiplier
 
 
 	
