96fdc113fb0d8a41ec381edc5d3164669563d0c7
NancokPS2 <nancok@tuta.io>
2023-01-26 20:00:09 -0300

    Mejorada HitBoxes

--- Clases/Areas/HitBox.gd
+++ Clases/Areas/HitBox.gd
@@ -1,41 +1,45 @@
 extends Area2D
 class_name Hitbox
 
-var activa:bool = false
-var poder:int = 1
-var objetivoValido:int = 0
+enum TiposObjetivos { ESTRUCTURA,JUGADOR,ENEMIGO,ENTIDAD}
 
-var cooldown:float = 1
+export (Array,NodePath) var immunes
+export (bool) var activa:bool = true
+export (int) var poder:int = 1
+export (TiposObjetivos,FLAGS) var objetivoValido:int
+export (bool) var autoActivar = true
+
+export (float) var cooldown = 1
 
 onready var temporizador:Timer = Timer.new()
 
-const TiposObjetivos = {
-	"ESTRUCTURA":1<<0,#Puertas, paredes, etc
-	"RIGIDO":1<<1,#Cajas, objetos varios rigidos
-	"JUGADOR":1<<2,#Solo el jugador
-	"ENTIDAD":1<<3#Entidades excluyendo el jugador
-}
+
 
 func _ready() -> void:
 	connect("body_entered",self,"trigger")
+	
+func _physics_process(delta: float) -> void:
+	if autoActivar:
+		pass
+		
 	
 func force_trigger():
 	for body in get_overlapping_bodies():
 		trigger(body)
 
-func trigger(target):
+func trigger(target:Node):
 	if not activa:#Si no esta activa, terminar aqui
 		return 
 		
 	temporizador.start(cooldown)
 	var lastimar:bool = false
-	if target is Jugador and objetivoValido && TiposObjetivos.JUGADOR:#Si es un jugador y tiene permitido dañarlo
+	if target.is_in_group("JUGADOR") and objetivoValido && TiposObjetivos.JUGADOR:#Si es un jugador y tiene permitido dañarlo
 		lastimar = true
 	
-	elif target is Entidad and objetivoValido && TiposObjetivos.ENTIDAD:
+	elif target.is_in_group("ENEMIGO") and objetivoValido && TiposObjetivos.ENEMIGO:
 		lastimar = true
 		
-	elif target is Rigido and objetivoValido && TiposObjetivos.RIGIDO:
+	elif target is Entidad and objetivoValido && TiposObjetivos.ENTIDAD:
 		lastimar = true
 		
 	elif target is Estructura and objetivoValido && TiposObjetivos.ESTRUCTURA:
