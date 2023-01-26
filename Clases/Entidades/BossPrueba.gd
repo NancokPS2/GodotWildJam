91d207e4e8bc9dc5f1c36f79ff6c16947f0dd17e
NancokPS2 <nancok@tuta.io>
2023-01-26 20:01:54 -0300

    Terminado primero intento del JefePrueba

--- Clases/Entidades/BossPrueba.gd
+++ Clases/Entidades/BossPrueba.gd
@@ -1,17 +1,43 @@
 extends Entidad
 class_name EntidadJefe
 
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 onready var jugador = Ref.jugador
 export (float) var velocidad = 50
 
 enum Estados {IDLE,CHASE,ATTACK}
 var estadoActual:int
 
+var subEstados = {
+	"atacando":false
+}
+
+
+
+func _ready() -> void:
+	animationPlayer.connect("animation_finished",self,"animation_ended")
+	add_to_group("ENEMIGO",true)
+	Ref.jefes.append(self)
+
+func animation_ended(nombreAnimacion:String):
+	decide_state()
+	pass
+
+
+var chaseDistance:float = 50
 func decide_state():
-	if position.distance_to(jugador.position) > 200:
+	
+	if jugador == null:
+		estadoActual = Estados.IDLE
+	
+	if position.distance_to(jugador.position) > chaseDistance:
 		estadoActual = Estados.CHASE
-	elif position.distance_to(jugador) <= 200:
+		
+	elif position.distance_to(jugador.position) <= chaseDistance:
 		estadoActual = Estados.ATTACK
+		
+	
+	entered_state()
 		
 
 		
@@ -20,16 +46,26 @@ func _physics_process(delta: float) -> void:
 	match estadoActual:
 		Estados.CHASE:
 			walk(position.direction_to(jugador.position))
+			animationPlayer.play("chase")
 		Estados.ATTACK:
 			pass
+		Estados.IDLE:
+			animationPlayer.play("idle")
+			if jugador:
+				decide_state()
+			
+		
+
 	
-func action():
+func entered_state():#Llamado UNA vez cuando se cambia de estado
 	match estadoActual:
 		Estados.ATTACK:
+			animationPlayer.play("attack")
+			
 			
 	
-func walk(direction:Vector2): 
-	move_and_slide(direction * velocidad)
+func walk(direccion:Vector2): 
+	move_and_slide(direccion * velocidad)
 	
 	
 	
