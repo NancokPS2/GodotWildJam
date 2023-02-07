6f00342bd5c6f71dd3f4d3affaedca9c17036736
NancokPS2 <nancok@tuta.io>
2023-02-07 19:33:08 -0300

    Renombradas algunas variables y funciones; Arreglado el movimiento del pañuelo; Arreglado el cambio de direccion del personaje (ahora usa "scale" en vez de girar sprites); Reconstruido el sistema de dash y salto;

--- Clases/Entidades/Jugador.gd
+++ Clases/Entidades/Jugador.gd
@@ -8,8 +8,7 @@ func _ready() -> void:
 
 	add_child(equipado)
 	Ref.jugador = self#Crear una referencia a si mismo
-	initial_count  = count
-	isAttacking = false
+	estadoAtaque = false
 	controlando = true
 	
 	gravedad = Const.gravedad
@@ -25,38 +24,42 @@ var inventario:Dictionary
 var armaEquipada
 
 # Movimiento
-var dash_velocity = 2000
-var dash_cooldown = 0
+var dashCooldownCurrent:float
+
+
 var speed:float = 200
-var jump_force:float = 340
 var gravity = 9.8
 var acceleration = 0.25
 var friction = 0.25
 
-var dash_count = 2
-var is_jumping = false
-# Whether the player is currently in the air
-var is_in_air = false
-export var count = 2
-var initial_count
+var estadoSalto:bool = false
+var estadoAire:bool = false
 # The upward velocity to apply for the jump
 var jump_velocity = 340
+var saltoRestantes:int
+var saltoCooldown:float
+
 onready var anim = $Anims
 onready var spritePlayer = $Sprite
-export var  isAttacking = false
+onready var colision = $Colision
+
+export var  estadoAtaque = false
 # The time remaining before the double jump can be used again (in seconds)
-var double_jump_cooldown = 0
 
 	
 func _physics_process(delta):
 	._physics_process(delta)#Proceso base de Entidad
 	
-	_on_floor_body_entered()#Checkear el estado actual antes de proseguir
+	surface_procs()#Checkear el estado actual antes de proseguir
+	
 	if controlando:
+		
 		# Decrement the dash cooldown
-		dash_cooldown = max(0, dash_cooldown - delta)
+		dashCooldownCurrent -= delta
 		# Decrement the Double jump cooldown
-		double_jump_cooldown = max(0, double_jump_cooldown - delta)
+		saltoCooldown -= delta
+		
+		dash()
 		side_movement()
 		_jump()
 		Animations()
@@ -65,51 +68,70 @@ func _physics_process(delta):
 	motion += Vector2.DOWN * gravedad * delta
 	motion = move_and_slide(motion, Vector2.UP)
 	
+	
+	#Pañuelo
 	var globalPlayerPosVec3 = Vector3(global_position.x,global_position.y,0)
 	$PlayerPoint/Camera.translation.x = globalPlayerPosVec3.x / get_viewport_rect().size.x
 	$PlayerPoint/Camera.translation.y = globalPlayerPosVec3.y / get_viewport_rect().size.y
 	$PlayerPoint/Camera.translation.z = 2.0
-	$PlayerPoint.move_and_slide( -Vector3(motion.x,motion.y,0) )
+	$PlayerPoint.move_and_slide( Vector3(motion.x,motion.y,0) * 0.05 )
 	
 #	$Debug.text = str(motion)
 	$Debug.text = str(global_position / get_viewport_rect().size)
 	
-	pass
+
+func dash():
+	if Input.is_action_just_pressed("dash") and dashCooldownCurrent <= 0:
+		var direccion:float = Input.get_axis("move_left","move_right")
+#	motion.x = direccion.x * dashDistance
+		move_and_slide( Vector2(direccion * estadisticas.dashDistance, 0) )
+		dashCooldownCurrent = estadisticas.dashCooldown
+
 
 func side_movement():
 	var direccionHorizontal = Input.get_axis("move_left","move_right")#Toma ambos botons como si fueran lados opuestos de una palanca
 	#direccionHorizontal resulta en -1 si es hacia la izquierda o 1 si a la derecha
-	if direccionHorizontal != 0 and !isAttacking:
+
+	if direccionHorizontal != 0 and !estadoAtaque:
 		motion.x = lerp(motion.x, direccionHorizontal * speed , acceleration)
 	else:
 		motion.x = lerp(motion.x, 0, friction)
 		
+	if motion.x > 0 and scale != Vector2(1,1) and not estadoAtaque:
+		scale = Vector2(1,1)
+		rotation = 0
+
+	elif motion.x < 0 and scale == Vector2(1,1) and not estadoAtaque:
+		scale = Vector2(-1,1)
+		
+		
+	
+
 func _jump():
 	if Input.is_action_just_pressed("jump"):
 		# If the player is on the ground, perform a jump
 		if is_on_floor():
-			motion = move_and_slide(Vector2(0, -jump_velocity))
-			is_jumping = true#Se inicio un salto
+			motion += move_and_slide(Vector2(0, -jump_velocity))
+			estadoSalto = true#Se inicio un salto
 			
 		# If the player is in the air and the double jump is available, perform a double jump
-		elif is_in_air and count > 0 and double_jump_cooldown == 0:
-			double_jump_cooldown = 0.15
-			is_jumping = true
-			count -=1
-			motion = move_and_slide(Vector2(0, -jump_velocity))
+		elif estadoAire and saltoRestantes > 0 and saltoCooldown <= 0:
+			saltoCooldown = 0.15
+			estadoSalto = true
+			saltoRestantes -= 1
+			motion += move_and_slide(Vector2(0, -jump_velocity))
 			
-func _on_floor_body_entered():
+func surface_procs():
 	if is_on_floor():
-		is_in_air = false
-		count = initial_count
-		is_jumping = false
+		estadoAire = false
+		saltoRestantes = 2
+		estadoSalto = false
 	else:
-		is_in_air = true
+		estadoAire = true
 		
 	if is_on_wall():
-		count = 1
-		is_jumping = false
-#This script will allow the player to 
+		saltoRestantes = 1
+		estadoSalto = false
 
 
 #func Attack():
@@ -132,11 +154,11 @@ func Animations():
 		
 	if motion.x > 3:#Yendo a la derecha
 		anim.play("correr")
-		spritePlayer.flip_h = false
+#		spritePlayer.flip_h = false
 		
 	elif motion.x < -3:#Izquierda
 		anim.play("correr")
-		spritePlayer.flip_h = true
+#		spritePlayer.flip_h = true
 		
 	else:#Si ninguna otra animacion califica
 		anim.play("idle")
